2416f663ed941cb9f42d8cc4bfb18450df9b7b5d
Paolo <p.degrazia@gmail.com>
2021-06-04 00:02:13 +0100

    completed migration

--- src/pages/ricambi.js
+++ src/pages/ricambi.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import Upgrades from '../components/Upgrades'
-import { upgradesData } from '../data/upgradesData';
+import { upgradesData } from '../data/ricambiData';
 
 
 export default function UpgradesPage() {
