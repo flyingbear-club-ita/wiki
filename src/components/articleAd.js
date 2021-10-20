32965fa7e880a8267d16543278e00e0f59baed9c
Paolo <p.degrazia@gmail.com>
2021-10-20 23:05:53 +0100

    stuff related to ads

---
+++ src/components/articleAd.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function ArticleAd() {
+    return(
+        <div className="articleAd">
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3253384067482467"
+            crossOrigin="anonymous"></script>
+            <ins className="adsbygoogle"
+                style={{display:"block", textAlign:"center"}}
+                data-ad-layout="in-article"
+                data-ad-format="fluid"
+                data-ad-client="ca-pub-3253384067482467"
+                data-ad-slot="5484842616">
+            </ins>
+            <script src="articleAdPush.js"></script>
+        </div>
+    );
+}
+
---
+++ static/anchorAd.js
@@ -0,0 +1,5 @@
+(adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-1234567891234567",
+    enable_page_level_ads: true,
+    overlays: {bottom: true}
+  });
