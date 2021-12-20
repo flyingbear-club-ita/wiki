a16d188eb78585a4b02f8160caea2600135f4e08
Paolo <p.degrazia@gmail.com>
2021-12-20 00:20:14 +0000

    added key to articleAd

--- src/components/articleAd.js
+++ src/components/articleAd.js
@@ -8,6 +8,7 @@ export default class ArticleAd extends React.Component {
 render () {
     return (
         <ins className='adsbygoogle'
+          key={Math.random()}
           style={{ display: 'block', textAlign:"center" }}
           data-ad-client='ca-pub-3253384067482467'
           data-ad-slot='5484842616'
